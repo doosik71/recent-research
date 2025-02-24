e9700ac12d733cdcc7efe69f9076770289a8cac0
Doosik Kim <doosik@etri.re.kr>
2025-02-24 10:02:16 +0900

    Add google button.

--- recent-research.js
+++ recent-research.js
@@ -19,21 +19,12 @@ function setInputValue(id, value) {
   document.getElementById(id).value = value;
 }
 
-function translateSummary(button) {
-  const summary = button.parentElement.previousElementSibling.textContent;
-  const summaryEscaped = encodeURIComponent(summary);
-  const url = `https://www.bing.com/translator?text=${summaryEscaped}&from=en&to=ko`;
-  window
-    .open(url, '_blank')
-    .focus();
-}
-
 async function getArxivList() {
   const arxivList = document.getElementById('arxiv-list');
   arxivList.innerHTML = 'Searching...';
 
   const researchTopic = document.getElementById('research-topic').value;
-  const max_results = 50;
+  const max_results = 5;
   const url = `https://export.arxiv.org/api/query?search_query=all:${researchTopic}&start=0&max_results=${max_results}&sortBy=relevance&sortOrder=descending`;
   const response = await fetch(url);
   const data = await response.text();
@@ -51,12 +42,13 @@ async function getArxivList() {
 
   for (let i = 0; i < entriesArray.length; i++) {
     const title = entriesArray[i].getElementsByTagName('title')[0].textContent;
-    const authorNodeList = entriesArray[i].getElementsByTagName('author');
-    const date = entriesArray[i].getElementsByTagName('published')[0].textContent.split('T')[0].replace(/-/g, '.');
-    const summary = entriesArray[i].getElementsByTagName('summary')[0].textContent;
-
+    const titleURI = encodeURIComponent(title);
     const link = entriesArray[i].getElementsByTagName('id')[0].textContent;
+    const authorNodeList = entriesArray[i].getElementsByTagName('author');
     const authors = Array.from(authorNodeList).map(a => a.getElementsByTagName('name')[0].textContent).join(', ');
+    const summary = entriesArray[i].getElementsByTagName('summary')[0].textContent;
+    const summaryURI = encodeURIComponent(summary);
+    const date = entriesArray[i].getElementsByTagName('published')[0].textContent.split('T')[0].replace(/-/g, '.');
     const pdf = link.replace('/abs/', '/pdf/');
 
     arxivList.innerHTML += `<div>
@@ -65,8 +57,9 @@ async function getArxivList() {
         <p id="summary" class="my-1">${summary}</p>
         <p class="my-2 text-right"><i>${date}</i></p>
         <div class="my-2 text-right">
-          <a href="${pdf}" target="_blank" class="pdf-button">PDF</a>
-          <button class="translate-button" type="button" onclick="translateSummary(this);">한</button>
+          <a href="${pdf}" target="_blank" class="pdf button-link">PDF</a>
+          <a href="https://scholar.google.com/scholar?q=${titleURI}" target="_blank" class="google button-link">G</a>
+          <a href="https://www.bing.com/translator?from=en&to=ko&text=${summaryURI}" target="_blank" class="translate button-link">한</a>
         </div>
         <hr/>
       </div>`;
